b1f1e8da86a0ba63cd693bfe42eae8c487837481
nicole1995wo-commits <nicole1995wo@gmail.com>
2026-01-02 12:35:35 +0400

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: "class",
-  content: ["./app/**/*.{ts,tsx}", "./lib/**/*.{ts,tsx}"],
-  theme: { extend: {} },
-  plugins: []
-};
